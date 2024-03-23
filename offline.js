0bf7aaa1c562e5537f9bfeb999935dc80cc6232e
testddoser <129606911+testddoser@users.noreply.github.com>
2024-03-23 21:53:07 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,16 @@
+﻿{
+	"version": 1711219937,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
